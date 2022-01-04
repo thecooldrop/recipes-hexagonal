7e4703e873c558cec7780920911b32dab4126bfe
Vanio Begic <vanio.begic123@gmail.com>
2022-01-04 23:12:05 +0100

    Bump the javax.measure API version to latest

--- src/main/java/udarnicka/recipes/ingredients/domain/WeightedIngredient.java
+++ src/main/java/udarnicka/recipes/ingredients/domain/WeightedIngredient.java
@@ -1,7 +1,27 @@
 package udarnicka.recipes.ingredients.domain;
 
-import javax.measure.Unit;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
 
-public class MeasuredIngredient<T extends Unit> {
-    
+import javax.measure.Quantity;
+import javax.measure.quantity.Mass;
+import java.util.Optional;
+
+@EqualsAndHashCode
+public class WeightedIngredient {
+
+    private final Ingredient ingredient;
+    private final Quantity<Mass> mass;
+
+    private WeightedIngredient(@NonNull Ingredient ingredient, @NonNull Quantity<Mass> mass) {
+        this.ingredient = ingredient;
+        this.mass = mass;
+    }
+
+    public Optional<WeightedIngredient> tryFrom(@NonNull Ingredient ingredient, @NonNull Quantity<Mass> mass) {
+        if(mass.getValue().doubleValue() <= 0) {
+            return Optional.empty();
+        }
+        return Optional.of(new WeightedIngredient(ingredient, mass));
+    }
 }

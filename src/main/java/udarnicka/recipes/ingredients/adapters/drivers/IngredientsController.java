599127f67a8a60d8bbf0708b0cd685266c76111f
Vanio Begic <vanio.begic123@gmail.com>
2022-01-08 13:00:41 +0100

    Add dependency of IngredientsController on IngredientsCrudUsecase

--- src/main/java/udarnicka/recipes/ingredients/adapters/drivers/IngredientsController.java
+++ src/main/java/udarnicka/recipes/ingredients/adapters/drivers/IngredientsController.java
@@ -1,7 +1,16 @@
 package udarnicka.recipes.ingredients.adapters.drivers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+import udarnicka.recipes.ingredients.domain.api.IngredientCrudUsecase;
 
 @RestController
 public class IngredientsController {
+
+    IngredientCrudUsecase ingredientsCrud;
+
+    @Autowired
+    public IngredientsController(IngredientCrudUsecase ingredientsCrud) {
+        this.ingredientsCrud = ingredientsCrud;
+    }
 }

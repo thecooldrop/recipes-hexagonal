9ff2e02c30d29ddf72e6f6b98b79c75d7e01d38b
Vanio Begic <vanio.begic123@gmail.com>
2022-02-05 00:49:32 +0100

    Configure the integration test for the testing of persistent repositories

--- src/test/java/udarnicka/ingredients/crud/persistence/jpa/JpaIngredientRepositoryTest.java
+++ src/test/java/udarnicka/ingredients/crud/persistence/jpa/JpaIngredientRepositoryTest.java
@@ -2,14 +2,33 @@ package udarnicka.ingredients.crud.persistence.jpa;
 
 import jdk.jfr.Description;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@DataJpaTest
 class JpaIngredientRepositoryTest {
 
     @Nested
-    @Description("If there is an H2 database")
-    public class GivenAnH2Database {
+    @Description("If there is an Postgresql database")
+    @DataJpaTest
+    @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+    @Testcontainers
+    public class GivenAPostgresqlDatabase {
+
+        @Container
+        private static PostgreSQLContainer container = new PostgreSQLContainer("postgres:14");
+
+        @DynamicPropertySource
+        static void registerProperties(DynamicPropertyRegistry registry) {
+            registry.add("spring.datasource.url", container::getJdbcUrl);
+            registry.add("spring.datasource.username", container::getUsername);
+            registry.add("spring.datasource.password", container::getPassword);
+        }
 
     }
 

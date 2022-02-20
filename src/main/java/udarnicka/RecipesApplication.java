0899d6d6480758e65d00e2c8f6311ce0464b35d0
Vanio Begic <vanio.begic123@gmail.com>
2022-02-20 22:09:03 +0100

    Enable configuring the local database based on environment variables

--- src/main/java/udarnicka/RecipesApplication.java
+++ src/main/java/udarnicka/RecipesApplication.java
@@ -4,17 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-import java.util.List;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class RecipesApplication {
 
-	private static final PostgreSQLContainer<?> localDatabase = new PostgreSQLContainer<>("postgres:14");
+	private static PostgreSQLContainer<?> localDatabase;
 
-	public static void main(String[] args) throws InterruptedException {
-		localDatabase.setPortBindings(List.of("12345:5432"));
-		localDatabase.start();
-		while(!localDatabase.isRunning());
+	public static void main(String[] args) {
+		if(System.getenv("RECIPES_LOCAL_DATABASE") != null) {
+			localDatabase = new PostgreSQLContainer<>("postgres:14");
+			localDatabase.setPortBindings(Arrays.asList(System.getenv("RECIPES_LOCAL_DATABASE_PORT_BINDINGS").split(",")));
+			localDatabase.start();
+			while(!localDatabase.isRunning());
+		}
 		SpringApplication.run(RecipesApplication.class, args);
 	}
 }

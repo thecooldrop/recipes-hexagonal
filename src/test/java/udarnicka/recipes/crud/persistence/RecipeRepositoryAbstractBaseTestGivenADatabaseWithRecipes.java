cc4f0263630882d94ff09136824a32dfbb612ee4
Vanio Begic <vanio.begic123@gmail.com>
2022-03-06 22:50:48 +0100

    Implement the abstract test suite for testing the recipe repositories
    We have created classes which contain the test, which should pass for all implementations of the
    RecipeRepository interface.

---
+++ src/test/java/udarnicka/recipes/crud/persistence/RecipeRepositoryAbstractBaseTestGivenADatabaseWithRecipes.java
@@ -0,0 +1,79 @@
+package udarnicka.recipes.crud.persistence;
+
+import jdk.jfr.Description;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.repository.CrudRepository;
+import udarnicka.common.HasCanonicalName;
+import udarnicka.common.HasId;
+import udarnicka.recipes.crud.domain.ports.CreateRecipe;
+import udarnicka.recipes.crud.domain.ports.Recipe;
+import udarnicka.recipes.crud.domain.ports.RecipeRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RecipeRepositoryAbstractBaseTestGivenADatabaseWithRecipes<Entity extends HasId<?> & HasCanonicalName, DataSource extends CrudRepository<Entity, ?>> {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    private List<Recipe> recipesInDb;
+
+    protected DataSource dataSource;
+    protected RecipeRepository recipeRepository;
+
+    protected void initializeData() {
+        recipesInDb = new ArrayList<>();
+        CreateRecipe pizzaMargharita = new CreateRecipe("Pizza Margarita");
+        CreateRecipe friedChickenBreast = new CreateRecipe("Fried Chicken Breast");
+        CreateRecipe frenchFries = new CreateRecipe("French Fries");
+        CreateRecipe lasagnaBolognese = new CreateRecipe("Lasagna Bolognese");
+        CreateRecipe spaghettiCarbonara = new CreateRecipe("Spaghetti Carbonara");
+
+        for(CreateRecipe command : Arrays.asList(pizzaMargharita, friedChickenBreast, frenchFries, lasagnaBolognese, spaghettiCarbonara)) {
+            Recipe recipe = recipeRepository.create(command);
+            recipesInDb.add(recipe);
+        }
+
+    }
+
+    @Test
+    @Description("then deleting a recipe removes it from the database")
+    protected void deletingTheRecipeRemovesItFromTheDatabase() {
+        initializeData();
+        Recipe randomRecipe = recipesInDb.get(new Random().nextInt(recipesInDb.size()));
+        long numOfRecipesInDbBeforeDeletion = dataSource.count();
+        Optional<Recipe> randomRecipeFromDb = recipeRepository.readById(randomRecipe.getId());
+        Optional<Recipe> removedRecipe = recipeRepository.deleteById(randomRecipe.getId());
+        long numOfRecipesInDatabaseAfterDeletion = dataSource.count();
+        em.flush();
+        em.clear();
+
+        Optional<Recipe> shouldBeEmptyRecipe = recipeRepository.readById(randomRecipe.getId());
+        assertThat(randomRecipeFromDb).isEqualTo(Optional.of(randomRecipe));
+        assertThat(removedRecipe).isEqualTo(randomRecipeFromDb);
+        assertThat(numOfRecipesInDatabaseAfterDeletion).isGreaterThan(numOfRecipesInDbBeforeDeletion);
+        assertThat(shouldBeEmptyRecipe).isEqualTo(Optional.empty());
+    }
+
+    @Test
+    @Description("then deleting a recipe returns the recipe")
+    protected void deletingTheRecipeReturnsDeletedRecipe() {
+        initializeData();
+        Recipe randomRecipe = recipesInDb.get(new Random().nextInt(recipesInDb.size()));
+        Optional<Recipe> recipeInDb = recipeRepository.deleteById(randomRecipe.getId());
+        assertThat(recipeInDb).isEqualTo(Optional.of(randomRecipe));
+    }
+
+    @Test
+    @Description("then the existing recipe can be read")
+    protected void existingRecipeCanBeRead() {
+        initializeData();
+        Recipe randomRecipe = recipesInDb.get(new Random().nextInt(recipesInDb.size()));
+        Optional<Recipe> recipeInDb = recipeRepository.readById(randomRecipe.getId());
+        assertThat(recipeInDb).isEqualTo(Optional.of(randomRecipe));
+    }
+}
---
+++ src/test/java/udarnicka/recipes/crud/persistence/RecipeRepositoryAbstractBaseTestGivenAnEmptyDatabase.java
@@ -0,0 +1,60 @@
+package udarnicka.recipes.crud.persistence;
+
+import jdk.jfr.Description;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.repository.CrudRepository;
+import udarnicka.common.CanonicalName;
+import udarnicka.common.HasCanonicalName;
+import udarnicka.common.HasId;
+import udarnicka.common.SerialInteger;
+import udarnicka.recipes.crud.domain.ports.CreateRecipe;
+import udarnicka.recipes.crud.domain.ports.DuplicateRecipeException;
+import udarnicka.recipes.crud.domain.ports.RecipeId;
+import udarnicka.recipes.crud.domain.ports.RecipeRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public abstract class RecipeRepositoryAbstractBaseTestGivenAnEmptyDatabase<Entity extends HasId<?> & HasCanonicalName, DataSource extends CrudRepository<Entity, ?>> {
+
+    protected RecipeRepository recipeRepository;
+    protected DataSource dataSource;
+
+    @Test
+    @Description("then new Recipes can be saved into the database")
+    protected void thenNewRecipesCanBeSavedIntoTheDatabase() {
+        CreateRecipe createRecipeCommand = new CreateRecipe("Pizza Margarita");
+        recipeRepository.create(createRecipeCommand);
+
+        int recipeCount = 0;
+        for(Entity recipeInDb : dataSource.findAll()) {
+            assertThat(recipeInDb.getId()).isNotNull();
+            assertThat(recipeInDb.getCanonicalName()).isEqualTo(new CanonicalName("Pizza Margarita"));
+            assertThat(recipeInDb.getCanonicalName().getOriginal()).isEqualTo("Pizza Margarita");
+            recipeCount++;
+        }
+
+        assertThat(recipeCount).isEqualTo(1);
+    }
+
+    @Test
+    @Description("then recipe with same canonical name can not be saved into database twice")
+    protected void recipeWithSameNameCanNotBeCreatedTwice() {
+        CreateRecipe createRecipe = new CreateRecipe("Pizza Margarita");
+        recipeRepository.create(createRecipe);
+        assertThatThrownBy(() -> recipeRepository.create(new CreateRecipe("pizza margarita")))
+                .isInstanceOf(DuplicateRecipeException.class);
+    }
+
+    @Test
+    @Description("then deleting any recipe returns an empty Optional")
+    protected void deletingAnyRecipeReturnsEmptyOptional() {
+        assertThat(recipeRepository.deleteById(new RecipeId(new SerialInteger(1)))).isEmpty();
+    }
+
+    @Test
+    @Description("then reading any ingredient returns an empty Optional")
+    protected void readingANonexistentRecipeReturnsAnEmptyOptional() {
+        assertThat(recipeRepository.readById(new RecipeId(new SerialInteger(1)))).isEmpty();
+    }
+}

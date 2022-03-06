65b6710274a2f2d1078334c5e4ed68dd340eaabb
Vanio Begic <vanio.begic123@gmail.com>
2022-03-06 23:01:12 +0100

    Implement the database entity for communicating the recipe data

--- src/main/java/udarnicka/recipes/crud/persistence/jpa/RecipeJpaEntity.java
+++ src/main/java/udarnicka/recipes/crud/persistence/jpa/RecipeJpaEntity.java
@@ -1,5 +1,7 @@
 package udarnicka.recipes.crud.persistence.jpa;
 
+import lombok.NonNull;
+import lombok.Setter;
 import udarnicka.common.CanonicalName;
 import udarnicka.common.HasCanonicalName;
 import udarnicka.common.HasId;
@@ -8,10 +10,12 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Table(name = "recipes")
 class RecipeJpaEntity implements HasId<Integer>, HasCanonicalName {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter
     private Integer id;
 
     @Column(name = "name", nullable = false)
@@ -22,13 +26,30 @@ class RecipeJpaEntity implements HasId<Integer>, HasCanonicalName {
     @NotBlank
     private String canonicalName;
 
+    private transient CanonicalName transientCanonicalName;
+
     @Override
     public CanonicalName getCanonicalName() {
-        return null;
+        return transientCanonicalName;
     }
 
     @Override
     public Integer getId() {
-        return null;
+        return id;
     }
+
+    void setName(@NonNull String name) {
+        if(name.isBlank()) {
+            throw new IllegalArgumentException("The name of the recipe may not be null or blank");
+        }
+        if(this.name != null) {
+            if(!transientCanonicalName.equals(new CanonicalName(name))) {
+                throw new IllegalArgumentException("The name of the recipe can not be changed. Only the casing of the letters in the recipe name may change.");
+            }
+        }
+        transientCanonicalName = transientCanonicalName == null ? new CanonicalName(name) : transientCanonicalName;
+        this.name = transientCanonicalName.getOriginal();
+        this.canonicalName = transientCanonicalName.getCanonical();
+    }
+
 }
--- src/main/resources/db/migration/V2__CreateRecipesTable.sql
+++ src/main/resources/db/migration/V2__CreateRecipesTable.sql
@@ -1,4 +1,4 @@
-create table recipes(
+create table recipes (
     id serial primary key,
     name text not null check (coalesce(trim(name),'') != ''),
     canonical_name text unique not null check (coalesce(trim(canonical_name), '') != '' and canonical_name = lower(name))

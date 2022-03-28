ddf5486600bf95095a050db64e4a20cf693aff73
Vanio Begic <vanio.begic123@gmail.com>
2022-03-28 21:00:19 +0200

    Add classes to represent different types of ingredients which can be included in recipes

---
+++ src/main/java/udarnicka/recipes/crud/domain/ports/VolumetricIngredients.java
@@ -0,0 +1,28 @@
+package udarnicka.recipes.crud.domain.ports;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
+import udarnicka.ingredients.crud.domain.ports.Ingredient;
+
+import javax.measure.quantity.Volume;
+
+/**
+ * Ingredients which are measured by volume. Example of such ingredients would be:
+ *
+ * - 500ml of water
+ * - 1L of chicken stock
+ * - 200ml of milk
+ */
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class VolumetricIngredients {
+
+    @NonNull
+    private final Volume volume;
+
+    @NonNull
+    private final Ingredient ingredient;
+}
---
+++ src/main/java/udarnicka/recipes/crud/domain/ports/WeightedIngredient.java
@@ -0,0 +1,20 @@
+package udarnicka.recipes.crud.domain.ports;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
+import udarnicka.ingredients.crud.domain.ports.Ingredient;
+
+import javax.measure.quantity.Mass;
+
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class WeightedIngredient {
+
+    @NonNull
+    private final Mass mass;
+
+    @NonNull
+    private final Ingredient ingredient;
+}

9c7a9cdf06cd92ce127d66be13b9e9a3f75a437f
Vanio Begic <vanio.begic123@gmail.com>
2022-05-05 20:37:56 +0200

    Make use of Lombok annotations

--- domain/src/main/java/ingredients/crud/api/IngredientId.java
+++ domain/src/main/java/ingredients/crud/api/IngredientId.java
@@ -2,6 +2,7 @@ package ingredients.crud.api;
 
 import common.PositiveInteger;
 import lombok.EqualsAndHashCode;
+import lombok.NonNull;
 
 import java.util.Objects;
 
@@ -10,23 +11,7 @@ public class IngredientId {
 
     private final PositiveInteger internal;
 
-    public IngredientId(PositiveInteger positiveInteger) {
-        if(positiveInteger == null) {
-            throw new NullPointerException("The IngredientId can not be initialized from null");
-        }
+    public IngredientId(@NonNull PositiveInteger positiveInteger) {
         this.internal = positiveInteger;
-    }
-
-    public boolean equals(Object o) {
-        if(this == o) {
-            return true;
-        }
-        if(o == null) {
-            return false;
-        }
-        if(!(o instanceof IngredientId)) {
-            return false;
-        }
-        return Objects.equals(this.internal, ((IngredientId) o).internal);
     }
 }

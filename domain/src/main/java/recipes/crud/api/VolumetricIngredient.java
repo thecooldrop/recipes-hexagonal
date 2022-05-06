63872a440eafe7a762f8f18b4977cbef9982f21c
Vanio Begic <vanio.begic123@gmail.com>
2022-05-06 23:36:35 +0200

    Complete the implementations of weighted and volumetric ingredients

--- domain/src/main/java/recipes/crud/api/VolumetricIngredient.java
+++ domain/src/main/java/recipes/crud/api/VolumetricIngredient.java
@@ -1,4 +1,18 @@
 package recipes.crud.api;
 
+import ingredients.crud.api.IngredientId;
+import lombok.NonNull;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Volume;
+
 public class VolumetricIngredient {
+
+    private final IngredientId id;
+    private final Quantity<Volume> volumeQuantity;
+
+    public VolumetricIngredient(@NonNull IngredientId id, @NonNull Quantity<Volume> volumeQuantity) {
+        this.id = id;
+        this.volumeQuantity = volumeQuantity;
+    }
 }
--- domain/src/main/java/recipes/crud/api/WeightedIngredient.java
+++ domain/src/main/java/recipes/crud/api/WeightedIngredient.java
@@ -1,4 +1,18 @@
 package recipes.crud.api;
 
+import ingredients.crud.api.IngredientId;
+import lombok.NonNull;
+
+import javax.measure.Quantity;
+import javax.measure.quantity.Mass;
+
 public class WeightedIngredient {
+
+    private final IngredientId id;
+    private final Quantity<Mass> massQuantity;
+
+    public WeightedIngredient(@NonNull IngredientId id, @NonNull Quantity<Mass> massQuantity) {
+        this.id = id;
+        this.massQuantity = massQuantity;
+    }
 }
